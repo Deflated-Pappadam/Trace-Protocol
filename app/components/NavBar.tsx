4a0ce0dedf64dcb0a81fc08f1f8da4c26500f4f9
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2024-03-23 15:39:49 +0530

    feat: profile page
    Co-authored-by: Rhon <Rhogneld@users.noreply.github.com>
    Co-authored-by: vinu b kurup <Hipster16@users.noreply.github.com>

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,14 +1,19 @@
+"use client";
 import React from "react";
 import { poppins } from "../lib/fonts";
 import Image from "next/image";
+import { useMetaMask } from "../hooks/useMetamask";
+import { formatAddress } from "../utils";
 
 function NavBar(props: { color: string }) {
   let logoColor = "#2f2079";
   let connected = false;
-
+  const { wallet, hasProvider, isConnecting, connectMetaMask } = useMetaMask();
+  const [_window, setWindowObject] = React.useState<any>(null);
   if (props.color === "#ffffff") {
     logoColor = "#ffffff";
   }
+
   return (
     <nav
       className={`mx-auto flex w-[85%] items-center justify-between p-4 text-black ${poppins.className}`}
@@ -28,7 +33,9 @@ function NavBar(props: { color: string }) {
             Home
           </a>
           <a
-            href="/#about" className="group rounded-full px-4 py-2 transition-all duration-150 hover:bg-[#e2dffe] hover:text-black">
+            href="/#about"
+            className="group rounded-full px-4 py-2 transition-all duration-150 hover:bg-[#e2dffe] hover:text-black"
+          >
             About
           </a>
           <a
@@ -38,12 +45,15 @@ function NavBar(props: { color: string }) {
             Explore
           </a>
           <a
-            href="/#contact" className="group rounded-full px-4 py-2 transition-all duration-150 hover:bg-[#e2dffe] hover:text-black">
+            href="/#contact"
+            className="group rounded-full px-4 py-2 transition-all duration-150 hover:bg-[#e2dffe] hover:text-black"
+          >
             Contact
           </a>
         </div>
       </div>
-      {connected ? (
+
+      {!hasProvider && (
         <button className="my-4  flex items-center justify-center rounded-full bg-[#ab9ff2] px-4 py-2 text-sm">
           <Image
             src="/metamask.png"
@@ -52,10 +62,17 @@ function NavBar(props: { color: string }) {
             height={20}
             className="m-1 mr-2"
           />
-           Profile
+          <a href="https://metamask.io" target="_blank">
+            Install MetaMask
+          </a>
         </button>
-      ) : (
-        <button className="my-4  flex items-center justify-center rounded-full bg-[#ab9ff2] px-4 py-2 text-sm">
+      )}
+      {hasProvider && wallet.accounts.length == 0 && (
+        <button
+          disabled={isConnecting}
+          onClick={connectMetaMask}
+          className="my-4  flex items-center justify-center rounded-full bg-[#ab9ff2] px-4 py-2 text-sm"
+        >
           <Image
             src="/metamask.png"
             alt=""
@@ -63,8 +80,25 @@ function NavBar(props: { color: string }) {
             height={20}
             className="m-1 mr-2"
           />
-         Connect Metamask
+          Connect Metamask
         </button>
+      )}
+      {hasProvider && wallet.accounts.length > 0 && (
+        <a
+          href={`/profile`}
+          // https://etherscan.io/address/${wallet.accounts[0]}
+          className="my-4  flex items-center justify-center rounded-full bg-[#ab9ff2] px-4 py-2 text-sm"
+        >
+          <Image
+            src="/metamask.png"
+            alt=""
+            width={20}
+            height={20}
+            className="m-1 mr-2"
+          />
+
+          {formatAddress(wallet.accounts[0],8)}
+        </a>
       )}
     </nav>
   );
---
+++ app/profile/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+import React from "react";
+import { inter, poppins } from "../lib/fonts";
+import NavBar from "../components/NavBar";
+import SaleBox from "../components/SaleBox";
+import Image from "next/image";
+import Avatars from "../components/avatar";
+import { useMetaMask } from "../hooks/useMetamask";
+import { formatAddress } from "../utils";
+
+function Page() {
+  const { wallet, hasProvider, isConnecting, connectMetaMask } = useMetaMask();
+  const [_window, setWindowObject] = React.useState<any>(null);
+  return (
+    <main
+      className={`flex min-h-screen w-full flex-col overflow-x-hidden bg-[#1c1c1c] ${poppins.className}`}
+    >
+      <NavBar color="#ffffff" />
+
+      <section className="flex min-h-[200px] w-full flex-col items-start justify-center">
+        <div className="flex flex-col w-[85%] mx-auto items-start  py-10">
+          <div className="py-5">
+          <Avatars />
+          </div>
+
+          {!hasProvider && (
+            <button className="my-4  flex items-center justify-center rounded-full bg-[#ab9ff2] px-4 py-2 text-sm">
+              <Image
+                src="/metamask.png"
+                alt=""
+                width={20}
+                height={20}
+                className="m-1 mr-2"
+              />
+              <a href="https://metamask.io" target="_blank">
+                Install MetaMask
+              </a>
+            </button>
+          )}
+          {hasProvider && wallet.accounts.length == 0 && (
+            <button
+              disabled={isConnecting}
+              onClick={connectMetaMask}
+              className="my-4  flex items-center justify-center rounded-full bg-[#ab9ff2] px-4 py-2 text-sm"
+            >
+              <Image
+                src="/metamask.png"
+                alt=""
+                width={20}
+                height={20}
+                className="m-1 mr-2"
+              />
+              Connect Metamask
+            </button>
+          )}
+          {hasProvider && wallet.accounts.length > 0 && (
+            <a
+              href={`/profile`}
+              // https://etherscan.io/address/${wallet.accounts[0]}
+              className="text-2xl"
+            >
+              {formatAddress(wallet.accounts[0], 16)}
+            </a>
+          )}
+        </div>
+      </section>
+      <section className="mx-auto w-[90%]">
+        <a href="" className="rounded-full bg-white px-4 py-2 text-black">
+          My Purchases
+        </a>
+        <div className="flex flex-wrap items-center gap-8 p-8  ">
+          <SaleBox
+            imgUrl="test.svg"
+            itemName="Something to sell"
+            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum est magni veniam tempore hic obcaecati!"
+          />
+          <SaleBox
+            imgUrl="test.svg"
+            itemName="Something to sell"
+            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum est magni veniam tempore hic obcaecati!"
+          />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Page;
